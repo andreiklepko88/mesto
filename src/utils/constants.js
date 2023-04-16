4eae8ee37be6429db7678c50e2eb37f61bbe8709
andreiklepko88 <118067419+andreiklepko88@users.noreply.github.com>
2023-04-16 23:41:19 +0300

    Первая итерация ПР9

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,33 +17,6 @@ export const config = {
   errorClass: 'popup__error'
 };
 
-// export const initialCards = [
-//   {
-//     name: 'Архыз',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//   },
-//   {
-//     name: 'Челябинская область',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//   },
-//   {
-//     name: 'Иваново',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//   },
-//   {
-//     name: 'Холмогорский район',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//   },
-//   {
-//     name: 'Байкал',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//   }
-// ];
-
 export const template = document.querySelector('.cards__template').content.querySelector('.cards__item');
 export const list = document.querySelector('.cards__list');
 export const inputPlace = document.querySelector('.popup__input_type_place');
